7b2424ab68372744241e7893c94a5e588a5875b3
Mohan Dev <bhardwaj1206409@gmail.com>
2024-08-03 22:42:32 +0530

    working on todo status in modal

--- src/components/FormElements/Select.tsx
+++ src/components/FormElements/Select.tsx
@@ -24,61 +24,75 @@ export const Select = ({
   };
 
   return (
-    <div className="relative inline-block text-left">
-      <div>
+    <div>
+      <label
+        id="listbox-label"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Status
+      </label>
+      <div className="relative mt-2">
         <button
           type="button"
-          className=" inline-flex min-h-40 w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          id="menu-button"
+          className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+          aria-haspopup="listbox"
           aria-expanded="true"
-          aria-haspopup="true"
+          aria-labelledby="listbox-label"
           onClick={toggleDropdown}
         >
-          {placeholder}
-          {
-            <svg
-              className="-mr-1 h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          }
+          <span className="flex items-center">
+            <span className="ml-3 block truncate">Tom Cook</span>
+          </span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"></span>
         </button>
-      </div>
-      {showDropdownOptions && (
-        <div
-          className="absolute right-0 left-0 z-10 mt-2 w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="menu-button"
-          tabIndex={-1}
-        >
-          {options?.map((ele, index) => (
-            <div
-              className="py-1"
-              role="none"
-              onClick={() => {
-                toggleDropdown();
-              }}
+
+        {/* <!--
+        Select popover, show/hide based on select state.
+  
+        Entering: ""
+          From: ""
+          To: ""
+        Leaving: "transition ease-in duration-100"
+          From: "opacity-100"
+          To: "opacity-0"
+      --> */}
+        {showDropdownOptions && (
+          <ul
+            className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            tabIndex={-1}
+            role="listbox"
+            aria-labelledby="listbox-label"
+            aria-activedescendant="listbox-option-3"
+          >
+            {/* <!--
+          Select option, manage highlight styles based on mouseenter/mouseleave and keyboard navigation.
+  
+          Highlighted: "bg-indigo-600 text-white", Not Highlighted: "text-gray-900"
+        --> */}
+            <li
+              className="relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900"
+              id="listbox-option-0"
+              role="option"
             >
-              <span
-                className="block px-4 py-2 text-sm text-gray-700 cursor-pointer"
-                role="menuitem"
-                tabIndex={-1}
-                id={`menu-item-${index}`}
-              >
-                {ele?.label}
-              </span>
-            </div>
-          ))}
-        </div>
-      )}
+              <div className="flex items-center">
+                {/* <!-- Selected: "font-semibold", Not Selected: "font-normal" --> */}
+                <span className="ml-3 block truncate font-normal">
+                  Wade Cooper
+                </span>
+              </div>
+
+              {/* <!--
+            Checkmark, only display for selected option.
+  
+            Highlighted: "text-white", Not Highlighted: "text-indigo-600"
+          --> */}
+              <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600"></span>
+            </li>
+
+            {/* <!-- More items... --> */}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
